a6f092fa0e90dcb0b412f88813d4138c18ea3a58
hahacold <taiwanhank0908@gmail.com>
2023-10-24 01:25:17 +0800

    新增中文,navigation觸發有待解決

--- nolanEbook/movieView.swift
+++ nolanEbook/movieView.swift
@@ -9,20 +9,18 @@ import SwiftUI
 struct movieMainPhoto: Identifiable {
     let id = UUID()
     let movie: String
-    //var movie: String
+    var zh: String
 }
 struct movieView: View {
+
     let moviePhotos = [
-        movieMainPhoto(movie: "tenet"),
-        movieMainPhoto(movie: "batman"),
-        movieMainPhoto(movie: "inception"),
-        movieMainPhoto(movie: "oppenheimer"),
-        movieMainPhoto(movie: "interstellar"),
-        movieMainPhoto(movie: "dunkirk")
-        
-        //BTSPhoto(name: "tenetbts0",movie: "tenet")
+        movieMainPhoto(movie: "tenet", zh: "天能"),
+        movieMainPhoto(movie: "batman", zh: "蝙蝠俠"),
+        movieMainPhoto(movie: "inception", zh: "全面啟動"),
+        movieMainPhoto(movie: "oppenheimer", zh: "奧本海默"),
+        movieMainPhoto(movie: "interstellar", zh: "星際效應"),
+        movieMainPhoto(movie: "dunkirk", zh: "敦克爾克大行動")
     ]
-    
     
     var ShuffledmoviePhoto: [movieMainPhoto] {
         return moviePhotos.shuffled()
@@ -43,7 +41,7 @@ struct movieView: View {
             ScrollView(.vertical ) {
                 
                 VStack(spacing:0){
-                    
+/*待研究文字為什麼不會跟著動
                     ForEach(ShuffledmoviePhoto) { moviePhoto in
                         //ZStack(){
                         NavigationLink {
@@ -64,14 +62,49 @@ struct movieView: View {
                                     .bold()
                                     .italic()
                                     .padding(10)
-                                    
+                                
                             }
                             
                         }
                     }
+ 
+ */
+                    
+                    
+                    ForEach(ShuffledmoviePhoto) { moviePhoto in
+                        //ZStack(){
+                        
+                        ZStack{
+                            Image(moviePhoto.movie)
+                                .resizable()
+                                .scaledToFill()
+                            
+                                .onTapGesture(count: 2){
+                                    show = !show
+                                }
+                            NavigationLink {
+                                whichview(movie: moviePhoto.movie)
+                            } label: {
+                                Text(moviePhoto.zh.capitalized)
+                                    .bold()
+                                    .italic()
+                                    .padding(10)
+                                    .background {
+                                        
+                                        RoundedRectangle(cornerRadius: 30)
+                                            .foregroundStyle(Gradient(colors: [.white,.gray]))
+                                    }
+                                
+                                
+                            }.offset(x:135,y:70)
+                            
+                        }.animation(.easeInOut(duration: 4), value: show)
+                            .onAppear{
+                                show = true
+                            }
+                    }
+                    
                 }
-                
-                
             }.background(Gradient(colors: [.black,.gray,.white]))
                 .ignoresSafeArea()
         }
