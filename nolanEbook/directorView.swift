764016e28f70cf28fc3ab39f5d2d2620587ebd7d
hahacold <taiwanhank0908@gmail.com>
2023-10-24 01:38:18 +0800

    加了導演敘述

--- nolanEbook/directorView.swift
+++ nolanEbook/directorView.swift
@@ -18,15 +18,19 @@ struct directorView: View {
         ZStack{
            LinearGradient(gradient: Gradient(colors: [.black, .gray, .white]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-            Image("nolan")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100)
-                .offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-                .animation(.easeInOut(duration: 4), value: show)
-                .onAppear{
-                    show = true
+            
+            HStack {
+                Image("nolan")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180)
+                    .offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                    .animation(.easeInOut(duration: 4), value: show)
+                    .onAppear{
+                        show = true
                 }
+                Text("克里斯多福·諾蘭是一位英裔美國電影導演，以複雜情節的好萊塢大片聞名，並被認為是21世紀頂級電影製作人。他的電影風格獨具特色，常常探討哲學、時間、記憶等主題，並以非線性敘事和後設元素為特色。他的作品充滿受到數學影響的圖像和概念、非常規的敘事結構、現場特效、實驗性音景、大畫幅膠片攝影和唯物主義角度。他的電影風格深受觀眾喜愛，並獲得了眾多獎項和提名")
+            }
         }
         
         
